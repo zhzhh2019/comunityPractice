9f1fc9fb1391ac91f2dc062b59fabcb6c62fd6e1
zzh19951227 <5624667752qq.com>
2020-05-16 22:27:14 +0800

    init practice

---
+++ src/main/java/conmunity/conmunity/ConmunityApplication.java
@@ -0,0 +1,13 @@
+package conmunity.conmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConmunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConmunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/conmunity/conmunity/ConmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package conmunity.conmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConmunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
