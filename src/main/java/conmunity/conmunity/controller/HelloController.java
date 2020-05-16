3e2568e1b058a28ef90fc00a818ceb58fc4290cc
zzh19951227 <5624667752qq.com>
2020-05-16 23:18:05 +0800

    create controller test

---
+++ src/main/java/conmunity/conmunity/controller/HelloController.java
@@ -0,0 +1,17 @@
+package conmunity.conmunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hell(String name, Model model){
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
